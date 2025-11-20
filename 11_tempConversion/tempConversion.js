1397deeb2e46d3471cf3df729aedb31973cb0af7
Tung Do <134229448+tungdo8842@users.noreply.github.com>
2025-11-19 23:31:32 -0800

    Finish exercises 6-11

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  return Number(((temp - 32) / 1.8).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  return Number((temp*1.8 + 32).toFixed(1));
 };
 
 // Do not edit below this line
